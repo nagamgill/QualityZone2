cf30104e94c323c2b022d919af82e3ecb72516dd
dmragar <dillon.ragar@colorado.edu>
2019-10-07 13:59:10 -0600

    dropbox path

--- GGL_NF_SP3.py
+++ GGL_NF_SP3.py
@@ -19,7 +19,7 @@ print("Checking Dropbox API")
 print(QualityZone.dbx.users_get_current_account())
 
 system_name = 'GGL_NF_SP3'
-dropbox_base = '/Boulder Creek CZO Team Folder/BcCZO'
+dropbox_base = '/CZO/BcCZO'
 master_file = '/Data/GordonGulch/GGL/GGL_NF_SP3/GGL_NF_SP3__CR10x_Excelandmeta/GGL_NF_SP3_CR10x_Master_WY2019.csv'
 new_file = '/Toughbook_Share/GordonGulch/GGL/Data/GGL_NF_SP3_Raw/GGL_NF_SP3_CR10xPB_15.dat'
 distribute_file = '/Data/GordonGulch/GGL/GGL_NF_SP3/GGL_NF_SP3__CR10x_Excelandmeta/GGL_NF_SP3_CR10x_Distribute_WY2019.csv'
@@ -127,7 +127,7 @@ df_master = QualityZone.download_master(master_path)
 df_new = QualityZone.download_new_data(new_path, newcols)
 df_updated = QualityZone.append_non_duplicates(df_master, df_new)
 
-working_file_path = '/Boulder Creek CZO Team Folder/BcCZO/Personnel_Folders/Dillon_Ragar/QualityZone/QZ_working_file.csv'
+working_file_path = os.path.join(dropbox_base + '/Personnel_Folders/Dillon_Ragar/QualityZone/QZ_working_file.csv')
 QualityZone.df_to_dropbox(df_updated, working_file_path)
 
 pecos.logger.initialize()
